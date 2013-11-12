22e5570956f34b4aba74c89b3e9a9a134d60865a
Ubaniak <bhavek.budhia@gmail.com>
2013-11-11 23:35:56 -0600

    created task model

---
+++ db/migrate/20131112053547_create_tasks.rb
@@ -0,0 +1,20 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+
+      t.string :title
+      t.string :description
+      t.datetime :due_date
+      t.integer :assigned_to
+      t.integer :approved_by
+      t.integer :reviewed_by
+      t.integer :created_by
+      t.datetime :reviewed_date
+      t.datetime :approved_date
+      t.integer :status_id
+      t.integer :department_id
+
+      t.timestamps
+    end
+  end
+end
