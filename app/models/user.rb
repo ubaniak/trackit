3453daee3c328aa70b582d31bdfd93988ce24e4f
Ubaniak <bhavek.budhia@gmail.com>
2013-11-12 04:50:40 -0600

    added hiarchy stuff to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.managers
+      User.where("manager = ? AND department_id = ?", true, self.department_id)
+  end
+
+  def self.subbordinates
+      User.where("manager = ? AND department_id = ?", false, self.department_id)
+  end
 end
