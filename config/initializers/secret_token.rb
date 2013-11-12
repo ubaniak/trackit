96f9ac1f8c05f4b517f2388aaa6cfddad49709ff
Ubaniak <bhavek.budhia@gmail.com>
2013-11-11 23:31:14 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackit::Application.config.secret_key_base = 'b914fc76e9d9e8e05800470146f6756ad3fd66db54f2cf8306f460a703964e127032f36af633e8a7bf6331b5635db3c247bc88ba137bf1993accdc37fd79aee1'
