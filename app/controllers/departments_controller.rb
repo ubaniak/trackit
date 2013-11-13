c157b4824635e2c1e484ff3f4c7ed724e531ae4c
Ubaniak <bhavek.budhia@gmail.com>
2013-11-13 17:54:55 -0600

    created depatrment admin and added department to users

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,7 @@
 class DepartmentsController < ApplicationController
+    def create
+        @department = Department.new(:name => params[:name])
+        @department.save
+        @departments = Department.all
+    end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,12 @@
 class UserController < ApplicationController
 
     def create
-        @user = User.new(:name => params[:user], :email => params[:email], :password => params[:password])
+        @user = User.new(:name => params[:user], 
+                         :email => params[:email], 
+                         :password => params[:password],
+                         :department_id => params[:department],
+                         :manager => params[:manager])
+
         @user.save
         @users = User.all
     end
